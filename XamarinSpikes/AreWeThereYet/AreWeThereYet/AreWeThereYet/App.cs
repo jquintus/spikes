28afca4377b835671c4099b783a9d8c2fae5aa3f
jquintus <josh.quintus@live.com>
2014-06-04 16:21:37 -0400

    Sample project playing with Xamarin forms

---
+++ XamarinSpikes/AreWeThereYet/AreWeThereYet/AreWeThereYet/App.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AreWeThereYet.Pages;
+using Xamarin.Forms;
+
+namespace AreWeThereYet
+{
+    public class App
+    {
+        public static Page GetMainPage()
+        {
+            return new MapPage();
+        }
+    }
+}
---
+++ XamarinSpikes/AreWeThereYet/AreWeThereYet/AreWeThereYet/Pages/MapPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreWeThereYet.Pages
+{
+    public partial class MapPage
+    {
+        public MapPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
