25fa1a8c42fc5b276b38589a6824a55f476ef9e8
jquintus <josh.quintus@live.com>
2014-06-04 16:21:14 -0400

    Sample project showing how to create swipeable views

---
+++ XamarinSpikes/DroidSpike/SwipeyView/DemoCollectionPagerAdapter.cs
@@ -0,0 +1,95 @@
+﻿using Android.OS;
+using Android.Support.V4.App;
+using Android.Views;
+using Android.Widget;
+using System;
+
+namespace SwipeyView
+{
+    public class DemoCollectionPagerAdapter : FragmentPagerAdapter
+    {
+        public DemoCollectionPagerAdapter(FragmentManager fm)
+            : base(fm)
+        {
+        }
+
+        public override int Count { get { return 4; } }
+
+        public override Fragment GetItem(int i)
+        {
+            switch (i)
+            {
+                case 0: return new TabOne();
+                case 1: return new TabTwo();
+                case 2: return new TabThree();
+                case 3: return new TabFour();
+                default: throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+
+    public class DemoObjectFragment : Fragment
+    {
+        public const string ARG_OBJECT = "object";
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // The last two arguments ensure LayoutParams are inflated properly
+            View rootView = inflater.Inflate(Resource.Layout.fragment_collection_object, container, false);
+
+            Bundle args = Arguments;
+            var view = rootView.FindViewById<TextView>(Android.Resource.Id.Text1);
+
+            view.Text = args.GetInt(ARG_OBJECT).ToString();
+            return rootView;
+        }
+    }
+
+    public abstract class JoshFragment : Fragment
+    {
+        private int _id;
+
+        public JoshFragment(int id)
+        {
+            _id = id;
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View rootView = inflater.Inflate(_id, container, false);
+            return rootView;
+        }
+    }
+
+    public class TabFour : JoshFragment
+    {
+        public TabFour()
+            : base(Resource.Layout.layout4)
+        {
+        }
+    }
+
+    public class TabOne : JoshFragment
+    {
+        public TabOne()
+            : base(Resource.Layout.layout1)
+        {
+        }
+    }
+
+    public class TabThree : JoshFragment
+    {
+        public TabThree()
+            : base(Resource.Layout.layout3)
+        {
+        }
+    }
+
+    public class TabTwo : JoshFragment
+    {
+        public TabTwo()
+            : base(Resource.Layout.layout2)
+        {
+        }
+    }
+}
---
+++ XamarinSpikes/DroidSpike/SwipeyView/MainActivity.cs
@@ -0,0 +1,86 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V4.App;
+using Android.Support.V4.View;
+
+namespace SwipeyView
+{
+    [Activity(Label = "SwipeyView", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : FragmentActivity
+    {
+        private DemoCollectionPagerAdapter mDemoCollectionPagerAdapter;
+        private ViewPager mViewPager;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+            var actionBar = ActionBar;
+
+            mDemoCollectionPagerAdapter = new DemoCollectionPagerAdapter(SupportFragmentManager);
+            mViewPager = FindViewById<ViewPager>(Resource.Id.pager);
+            mViewPager.Adapter = mDemoCollectionPagerAdapter;
+
+            mViewPager.SetOnPageChangeListener(new OnPageChangeListener(actionBar));
+
+            actionBar.NavigationMode = ActionBarNavigationMode.Tabs;
+
+            ActionBar.ITabListener tabListener = new TabListener(mViewPager);
+
+            for (int i = 0; i < mDemoCollectionPagerAdapter.Count; i++)
+            {
+                actionBar.AddTab(
+                        actionBar.NewTab()
+                                .SetText("Tab " + (i + 1))
+                                .SetTabListener(tabListener));
+            }
+        }
+    }
+
+    public class OnPageChangeListener : Java.Lang.Object, Android.Support.V4.View.ViewPager.IOnPageChangeListener
+    {
+        private ActionBar mActionBar;
+
+        public OnPageChangeListener(ActionBar ab)
+        {
+            mActionBar = ab;
+        }
+
+        public void OnPageScrolled(int position, float positionOffset, int positionOffsetPixels)
+        {
+        }
+
+        public void OnPageScrollStateChanged(int state)
+        {
+        }
+
+        public void OnPageSelected(int position)
+        {
+            mActionBar.SetSelectedNavigationItem(position);
+        }
+    }
+
+    public class TabListener : Java.Lang.Object, ActionBar.ITabListener
+    {
+        private ViewPager mViewPager;
+
+        public TabListener(ViewPager vp)
+        {
+            mViewPager = vp;
+        }
+
+        public void OnTabReselected(ActionBar.Tab tab, Android.App.FragmentTransaction ft)
+        {
+        }
+
+        public void OnTabSelected(ActionBar.Tab tab, Android.App.FragmentTransaction ft)
+        {
+            mViewPager.SetCurrentItem(tab.Position, true);
+        }
+
+        public void OnTabUnselected(ActionBar.Tab tab, Android.App.FragmentTransaction ft)
+        {
+        }
+    }
+}
