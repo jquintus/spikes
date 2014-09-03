825f9e931b884d11ab7d8e92f36503bfba851f1b
jquintus <josh.quintus@live.com>
2014-09-03 16:43:04 -0400

    Creating a tool to start an app and then just exit.

---
+++ XamarinSpikes/Launcher/Launcher/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+
+namespace Launcher
+{
+    [Activity(Label = "Yarly Launcher", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+
+            Intent launchIntent = PackageManager.GetLaunchIntentForPackage("co.yarly.droid");
+            StartActivity(launchIntent);
+
+            Finish();
+        }
+    }
+}
