4f596fb64f78b6b6c6f680c7d76cd980d4bdad27
jquintus <josh.quintus@live.com>
2014-09-25 12:21:21 -0400

    Playing around with layout transitions.  Easier than I had thought.

--- XamarinSpikes/DroidSpike/LayoutAnimations/MainActivity.cs
+++ XamarinSpikes/DroidSpike/LayoutAnimations/MainActivity.cs
@@ -1,31 +1,73 @@
-﻿using System;
+﻿using Android.Animation;
 using Android.App;
-using Android.Content;
-using Android.Runtime;
+using Android.OS;
 using Android.Views;
 using Android.Widget;
-using Android.OS;
+using Java.Interop;
+using System;
 
 namespace LayoutAnimations
 {
     [Activity(Label = "LayoutAnimations", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
+        private LinearLayout _ll;
+        private Tag _tag;
+        private int count = 1;
+
+        public TextView _tv { get; set; }
+
+        [Export("myOnClick")]
+        public void myOnClick(View v)
+        {
+            DoStuff();
+        }
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            _ll = FindViewById<LinearLayout>(Resource.Id.myview);
 
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+
+            var tx = new LayoutTransition();
+            _ll.LayoutTransition = tx; // This is the magic.  So easy.
+
+
+            _tag = new Tag("tag");
+            _tv = new TextView(this)
+            {
+                Text = "Now I'm here",
+                Tag = _tag,
+            };
+        }
+
+        private void DoStuff()
+        {
+            Toast toast = Toast.MakeText(this, string.Format("You clicked {0} times", ++count), ToastLength.Long);
+            toast.Show();
+
+            var existingView = _ll.FindViewWithTag(_tag);
+            if (existingView == null)
+            {
+                _ll.AddView(_tv, 2);
+            }
+            else
+            {
+                _ll.RemoveView(_tv);
+            }
+        }
+
+        public class Tag : Java.Lang.Object
+        {
+            public Tag(string str)
+            {
+                TagValue = str;
+            }
+
+            public string TagValue { get; set; }
         }
     }
 }
-
