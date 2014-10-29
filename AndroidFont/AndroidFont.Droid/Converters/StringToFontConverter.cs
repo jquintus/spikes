53476991eeb4c768f47e401d3e7ccfe6d4191050
jquintus <josh.quintus@live.com>
2014-10-29 18:12:08 -0400

    Got the converter to work.  Nice and neat.

--- AndroidFont/AndroidFont.Droid/Converters/StringToFontConverter.cs
+++ AndroidFont/AndroidFont.Droid/Converters/StringToFontConverter.cs
@@ -2,14 +2,34 @@ using Android.App;
 using Android.Graphics;
 using Cirrious.CrossCore.Converters;
 using System;
+using System.Collections.Generic;
 
 namespace AndroidFont.Droid.Converters
 {
     public class StringToFontConverter : MvxValueConverter<string, Typeface>
     {
-        protected override Typeface Convert(string value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        private static Dictionary<string, Typeface> _cache = new Dictionary<string, Typeface>();
+
+        protected override Typeface Convert(string fontName, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            return Typeface.CreateFromAsset(Application.Context.Assets, "font/Fontin-Italic.ttf");
+            try
+            {
+                if (!fontName.StartsWith(@"font/")) fontName = @"font/" + fontName;
+                if (!fontName.EndsWith(".ttf")) fontName += ".ttf";
+
+                if (!_cache.ContainsKey(fontName))
+                {
+                    _cache[fontName] = Typeface.CreateFromAsset(Application.Context.Assets, fontName);
+                }
+
+                return _cache[fontName];
+            }
+            catch (Exception e)
+            {
+                Android.Util.Log.Error("AndroidFont", e.ToString());
+
+                return Typeface.Default;
+            }
         }
     }
 }
--- AndroidFont/AndroidFont.Droid/SplashScreen.cs
+++ AndroidFont/AndroidFont.Droid/SplashScreen.cs
@@ -5,12 +5,12 @@ using Cirrious.MvvmCross.Droid.Views;
 namespace AndroidFont.Droid
 {
     [Activity(
-		Label = "AndroidFont.Droid"
-		, MainLauncher = true
-		, Icon = "@drawable/icon"
-		, Theme = "@style/Theme.Splash"
-		, NoHistory = true
-		, ScreenOrientation = ScreenOrientation.Portrait)]
+        Label = "AndroidFont.Droid"
+        , MainLauncher = true
+        , Icon = "@drawable/icon"
+        , Theme = "@style/Theme.Splash"
+        , NoHistory = true
+        , ScreenOrientation = ScreenOrientation.Portrait)]
     public class SplashScreen : MvxSplashScreenActivity
     {
         public SplashScreen()
