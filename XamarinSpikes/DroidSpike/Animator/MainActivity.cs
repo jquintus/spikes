3fded593f2735bee4d76d28d34b937cb8180067a
Josh <josh.quintus@live.com>
2015-01-15 16:27:12 -0500

    Playing with animations.

---
+++ XamarinSpikes/DroidSpike/Animator/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace Animator
+{
+    [Activity(Label = "Animator", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
