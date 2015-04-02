dd556a3ed5ad4a6db0dcc2f3d43be6d9f8714efa
jquintus <josh@dotalign.com>
2015-04-02 14:21:22 -0400

    Create reusable spike console.

---
+++ ConsoleApps/FunWithSpikes/FunWithSpikes/Disposable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FunWithSpikes
+{
+    public class Disposable : IDisposable
+    {
+        public static void FunWithDisposable()
+        {
+            var d = new Disposable();
+            using (d)
+            {
+                d = null;
+            }
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Disposed");
+        }
+    }
+}
