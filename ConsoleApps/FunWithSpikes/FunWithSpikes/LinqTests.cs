60681ffc6e5f22aea37e0d3d83d3a37fc4dfa007
jquintus <josh@dotalign.com>
2015-05-28 14:28:15 -0400

    I thought i was going to fiddle.

---
+++ ConsoleApps/FunWithSpikes/FunWithSpikes/LinqTests.cs
@@ -0,0 +1,16 @@
+﻿namespace FunWithSpikes
+{
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+
+    [TestFixture]
+    public class LinqTests
+    {
+
+    }
+}
