8632b6032e41abeba9e5f41c3bf989d7fab4f427
jquintus <josh@dotalign.com>
2015-06-03 17:18:05 -0400

    Testing Log4Net

---
+++ ConsoleApps/FunWithSpikes/FunWithLogs/Program.cs
@@ -0,0 +1,24 @@
+﻿using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithLogs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlConfigurator.Configure(new FileInfo("log4net.config"));
+
+
+            Console.WriteLine(Path.Combine(@"C:\folder", @"folder2"));
+
+            Console.ReadLine();
+
+        }
+    }
+}
