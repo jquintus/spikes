069ff939c33c7827d89dbb4252b1085b2224c1d8
jquintus <josh@dotalign.com>
2015-06-03 17:17:45 -0400

    Testing whether or not File.Exists will fail to find a path if it includes double backslashes.

---
+++ ConsoleApps/FunWithSpikes/ExistentialFiles/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace ExistentialFiles
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            if (File.Exists(@"c:\path\\\\\\\\\\file.txt"))
+            {
+                Console.WriteLine(@"File.Exists NO cares about \\");
+            }
+            else
+            {
+                Console.WriteLine(@"File.Exists cares about \\ !!!!!!!!!!!!!!!!");
+            }
+        }
+    }
+}
---
+++ ConsoleApps/FunWithSpikes/ExistentialFiles/readme.md
@@ -0,0 +1,1 @@
+﻿Testing whether or not File.Exists will fail to find a path if it includes double backslashes.  
