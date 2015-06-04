375b5a0e4967dff0dce863241162d0a72df23397
jquintus <josh@dotalign.com>
2015-06-04 13:03:25 -0400

    Ken's custom action code.

---
+++ ConsoleApps/FunWithSpikes/Wix.CustomActions/CustomAction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Deployment.WindowsInstaller;
+
+namespace Wix.CustomActions
+{
+    public class CustomActions
+    {
+        [CustomAction]
+        public static ActionResult CloseIt(Session session)
+        {
+            try
+            {
+                session.Log("Close DotTray!");
+            }
+            catch (Exception ex)
+            {
+                session.Log("ERROR in custom action CloseIt {0}", ex.ToString());
+                return ActionResult.Failure;
+            }  
+            return ActionResult.Success;
+        }
+    }
+}
