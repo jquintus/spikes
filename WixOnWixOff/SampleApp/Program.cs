58eb67c5c011dd5c1dd2025f5ea5ad4f81f3711c
jquintus <josh@dotalign.com>
2015-06-07 10:04:04 -0400

    Create wix sample solution.

---
+++ WixOnWixOff/SampleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SampleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Press Enter to Terminate");
+            Console.ReadLine();
+        }
+    }
+}
