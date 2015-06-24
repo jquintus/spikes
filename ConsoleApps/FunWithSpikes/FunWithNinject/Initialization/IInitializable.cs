03300ba99a6791175bbd7dabf03bf220439de5a5
jquintus <josh@dotalign.com>
2015-06-24 11:00:41 -0400

    OnActivation

---
+++ ConsoleApps/FunWithSpikes/FunWithNinject/Initialization/IInitializable.cs
@@ -0,0 +1,26 @@
+﻿namespace FunWithNinject.Initialization
+{
+    public interface IInitializable
+    {
+        bool IsInit { get; }
+
+        void Init();
+    }
+
+    public class Initializable : IInitializable
+    {
+        public bool CalledMe { get; set; }
+
+        public bool IsInit { get; set; }
+
+        public void CallMe()
+        {
+            CalledMe = true;
+        }
+
+        public void Init()
+        {
+            IsInit = true;
+        }
+    }
+}
---
+++ ConsoleApps/FunWithSpikes/FunWithNinject/Initialization/Tests.cs
@@ -0,0 +1,41 @@
+﻿namespace FunWithNinject.Initialization
+{
+    using Ninject;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void OnActivation_CanCallMethodOnImplementation()
+        {
+            // Assemble
+            var k = new StandardKernel();
+            k.Bind<IInitializable>()
+                .To<Initializable>()
+                .OnActivation(i => i.CallMe());
+
+            // Act
+            var initted = k.Get<IInitializable>();
+
+            // Assert
+            Assert.IsTrue(((Initializable)initted).CalledMe);
+        }
+
+        [Test]
+        public void OnActivation_CanCallMethodOnInterface()
+        {
+            // Assemble
+            var k = new StandardKernel();
+            k.Bind<IInitializable>()
+                .To<Initializable>()
+                .OnActivation(i => i.Init());
+
+            // Act
+            var initted = k.Get<IInitializable>();
+
+            // Assert
+            Assert.IsTrue(initted.IsInit);
+        }
+    }
+}
