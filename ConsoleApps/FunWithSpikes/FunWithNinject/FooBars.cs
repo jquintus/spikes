92d796848afded53db12adee05e59b5f54143cac
jquintus <josh@dotalign.com>
2015-06-24 09:50:26 -0400

    Forgot to include in last checkin.

---
+++ ConsoleApps/FunWithSpikes/FunWithNinject/FooBars.cs
@@ -0,0 +1,32 @@
+﻿namespace FunWithNinject
+{
+    /* 
+     * So I don't have to create a whole bunch of dependencies
+     * each time I want to create a new test, this file 
+     * contains a few simple interfaces with some dependencies.
+     * 
+     * If anything more complicated is needed, it will be a good
+     * idea to create new classes/dependencies specifically for 
+     * the tests that require them.
+     */ 
+
+    public interface IBar { }
+
+    public interface IFoo { }
+
+    public class Bar : IBar { }
+
+    public class Foo : IFoo
+    {
+        public Foo(IBar bar)
+        {
+            Bar = bar;
+        }
+
+        public IBar Bar { get; set; }
+    }
+
+    public class Foo1 : IFoo { }
+
+    public class Foo2 : IFoo { }
+}
