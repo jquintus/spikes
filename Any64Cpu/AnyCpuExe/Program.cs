06792a0577f2264ff22dcdd1b9b19ad2b86edc3b
jquintus <josh@dotalign.com>
2015-06-26 16:21:38 -0400

    Create apps that tell you what bitness they are.

---
+++ Any64Cpu/AnyCpuExe/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+namespace AnyCpuExe
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var asm = Assembly.GetEntryAssembly();
+            PortableExecutableKinds peKind;
+            ImageFileMachine imageFileMachine;
+
+            asm.ManifestModule.GetPEKind(out peKind, out imageFileMachine);
+
+            Console.WriteLine("{0} {1}", asm.ManifestModule.Name, imageFileMachine);
+        }
+    }
+}
