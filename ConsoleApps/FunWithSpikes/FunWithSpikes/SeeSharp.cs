75fd32f39c94e42d1d52719ddf19db40da91b8d3
jquintus <josh@dotalign.com>
2015-07-09 15:05:02 -0400

    Throwing an exception after setting an out parameter still leaves the paramter set.

---
+++ ConsoleApps/FunWithSpikes/FunWithSpikes/SeeSharp.cs
@@ -0,0 +1,40 @@
+﻿namespace FunWithSpikes
+{
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [TestFixture]
+    public class SeeSharp
+    {
+        [Test]
+        public void CallAMethodWithAnOutParam_OutParamIsSetAndExceptionThrown_CanAccessOutParamsValueFromCaller()
+        {
+            // Assemble
+            string msg = "Not set";
+            bool caught = false;
+
+            // Act
+            try
+            {
+                SetsOutAndThrows(out msg);
+            }
+            catch {
+                caught = true;
+            }
+
+            // Assert
+            Assert.AreEqual("Hello World", msg);
+            Assert.IsTrue(caught);
+        }
+
+        private void SetsOutAndThrows(out string msg)
+        {
+            msg = "Hello World";
+            throw new Exception();
+        }
+    }
+}
