94d488638719e3c46446c78732d3de89a4e46c7d
jquintus <josh@dotalign.com>
2015-08-07 13:53:23 -0400

    Show how binding open generics work

---
+++ ConsoleApps/FunWithSpikes/FunWithNinject/OpenGenerics/OpenGenericsTests.cs
@@ -0,0 +1,52 @@
+﻿using Ninject;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithNinject.OpenGenerics
+{
+    [TestFixture]
+    public class OpenGenericsTests
+    {
+
+        public interface ILogger<T>
+        {
+            Type GenericParam { get; }
+        }
+        public class Logger<T> : ILogger<T>
+        {
+            public Type GenericParam { get { return typeof(T); } }
+
+        }
+
+        public class DependsOnLogger
+        {
+            public DependsOnLogger(ILogger<int> intLogger)
+            {
+                GenericParam = intLogger.GenericParam;
+            }
+
+            public Type GenericParam { get; set; }
+
+        }
+
+        [Test]
+        public void OpenGenericBinding()
+        {
+            using (var k = new StandardKernel())
+            {
+                // Assemble
+                k.Bind(typeof(ILogger<>)).To(typeof(Logger<>));
+
+                // Act
+                var dependsOn = k.Get<DependsOnLogger>();
+
+                // Assert
+                Assert.AreEqual(typeof(int), dependsOn.GenericParam);
+            }
+        }
+    }
+}
