ca4465228d299fcf5056e66b051cd8b7b0e95cd0
jquintus <josh@dotalign.com>
2015-09-29 17:16:57 -0400

    Mocking out an abstract class.

---
+++ ConsoleApps/FunWithSpikes/FunWithMoq/AbstractMoqTests.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithMoq
+{
+    [TestFixture]
+    public class AbstractMoqTests
+    {
+        public abstract class MyClass
+        {
+            public abstract string MyString { get; }
+
+            public string GetMyString()
+            {
+                return MyString;
+            }
+        }
+
+        [Test]
+        public void SetupGet_AbstractProperty_ValueIsReturnedWhenCalledByAnotherMember()
+        {
+            // Assemble
+            var mock = Mock.Of<MyClass>(c => c.MyString == "Hello");
+
+            // Act
+            var actual = mock.GetMyString();
+
+            // Assert
+            Assert.AreEqual("Hello", actual);
+        }
+    }
+}
