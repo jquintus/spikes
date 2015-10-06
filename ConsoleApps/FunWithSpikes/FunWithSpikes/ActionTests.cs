3cb687fc5cc4851a544e69bb4805fbf9f7bf7726
jquintus <josh@dotalign.com>
2015-10-06 09:32:17 -0400

    Blowing my mind with action+=

---
+++ ConsoleApps/FunWithSpikes/FunWithSpikes/ActionTests.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace FunWithSpikes
+{
+    [TestFixture]
+    public class ActionTests
+    {
+        [Test]
+        public void MinusEquals_AfterFewerPlusEquals_RemovesAllLambdas()
+        {
+            // Assemble
+            int lambdaCount = 0;
+            int actionCount = 0;
+
+            var lambda1 = new Action(() => lambdaCount++);
+            Action jackson = new Action(() => actionCount++);
+
+            // Act
+            jackson += lambda1;
+            jackson -= lambda1;
+            jackson -= lambda1;
+            jackson();
+
+            // Assert
+            Assert.AreEqual(0, lambdaCount);
+            Assert.AreEqual(1, actionCount);
+        }
+
+        [Test]
+        public void MinusEquals_AfterMorePlusEquals_RemovesAddedLambdas()
+        {
+            // Assemble
+            int lambdaCount = 0;
+            int actionCount = 0;
+            var lambda1 = new Action(() => lambdaCount++);
+
+            Action jackson = new Action(() => actionCount++);
+
+            jackson();
+
+            // Act
+            jackson += lambda1;
+            jackson += lambda1;
+            jackson -= lambda1;
+            jackson();
+
+            // Assert
+            Assert.AreEqual(3, lambdaCount);
+            Assert.AreEqual(2, actionCount);
+        }
+
+        [Test]
+        public void PlusEquals_RunsMultipleLambdas()
+        {
+            // Assemble
+            int lambdaCount = 0;
+            int actionCount = 0;
+            var lambda1 = new Action(() => lambdaCount++);
+
+            // Act
+            Action jackson = new Action(() => actionCount++);
+            jackson += lambda1;
+            jackson += lambda1;
+
+            jackson();
+
+            // Assert
+            Assert.AreEqual(2, lambdaCount);
+            Assert.AreEqual(1, actionCount);
+        }
+    }
+}
--- ConsoleApps/FunWithSpikes/FunWithSpikes/SeeSharp.cs
+++ ConsoleApps/FunWithSpikes/FunWithSpikes/SeeSharp.cs
@@ -1,12 +1,8 @@
-﻿namespace FunWithSpikes
-{
-    using NUnit.Framework;
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+﻿using NUnit.Framework;
+using System;
 
+namespace FunWithSpikes
+{
     [TestFixture]
     public class SeeSharp
     {
@@ -22,7 +18,8 @@
             {
                 SetsOutAndThrows(out msg);
             }
-            catch {
+            catch
+            {
                 caught = true;
             }
 
