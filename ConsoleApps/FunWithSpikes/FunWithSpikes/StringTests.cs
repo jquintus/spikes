4dc822b4739ef44cade9bf6d7b8bcc9b4b2ced89
jquintus <josh@dotalign.com>
2015-10-13 12:49:10 -0400

    String interpolation tests.

---
+++ ConsoleApps/FunWithSpikes/FunWithSpikes/StringTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace FunWithSpikes
+{
+    [TestFixture]
+    public class StringTests
+    {
+        [Test]
+        public void StringInterpolation_PassInAnObject_ImplicitlyCallsToString()
+        {
+            // Assemble
+            var mc = new MyClass();
+
+            // Act
+            var actual = $"{mc} world";
+
+            // Assert
+            Assert.AreEqual("hello world", actual);
+        }
+
+        private class MyClass
+        {
+            public override string ToString()
+            {
+                return "hello";
+            }
+        }
+    }
+}
