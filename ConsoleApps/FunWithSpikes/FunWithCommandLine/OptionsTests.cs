0f0670d7a58e6b56f865281635357a0c612a599f
jquintus <josh@dotalign.com>
2015-12-23 17:45:39 -0500

    Add spikes for commandLineParser

---
+++ ConsoleApps/FunWithSpikes/FunWithCommandLine/OptionsTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace FunWithCommandLine
+{
+    [TestFixture]
+    public class OptionsTests
+    {
+        [Test]
+        public void OptionsWithDefaultEnum_EmptyArgsPassed_ReturnsDefaultValue()
+        {
+            // Assemble
+            var args = new string[0];
+
+            // Act
+            var options = CreateOptions<OptionsWithDefaultEnum>(args);
+
+            // Assert
+            Assert.AreEqual(Letter.B, options.DefaultedLetter);
+        }
+
+        [Test]
+        public void OptionsWithEnums_EmptyArgsPassed_ReturnsDefaultValue()
+        {
+            // Assemble
+            var args = new string[0];
+
+            // Act
+            var options = CreateOptions<OptionsWithEnums>(args);
+
+            // Assert
+            Assert.AreEqual(Letter.A, options.DefaultedLetter);
+        }
+
+        private T CreateOptions<T>(string[] args) where T : new()
+        {
+            var options = new T();
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                return options;
+            }
+            else
+            {
+                Assert.Fail("could not parse args");
+                throw new Exception("this will never be hit since Assert.Fail throws internally");
+            }
+        }
+    }
+}
---
+++ ConsoleApps/FunWithSpikes/FunWithCommandLine/OptionsWithDefaultEnum.cs
@@ -0,0 +1,24 @@
+﻿using CommandLine;
+
+namespace FunWithCommandLine
+{
+    public enum Letter
+    {
+        A,
+        B,
+        C,
+    }
+
+    public class OptionsWithDefaultEnum
+    {
+        [Option("letter", DefaultValue = Letter.B)]
+        public Letter DefaultedLetter { get; set; }
+    }
+
+
+    public class OptionsWithEnums
+    {
+        [Option("letter")]
+        public Letter DefaultedLetter { get; set; }
+    }
+}
