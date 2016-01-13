a5c96fe96e4cb4b8bc1539eee24e6f6c37fda2f4
jquintus <josh@dotalign.com>
2016-01-13 15:11:32 -0500

    Add tests for json lists.

--- ConsoleApps/FunWithSpikes/FunWithNewtonsoft/ListTests.cs
+++ ConsoleApps/FunWithSpikes/FunWithNewtonsoft/ListTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
 using System.Collections.Generic;
 
 namespace FunWithNewtonsoft
@@ -7,16 +8,56 @@ namespace FunWithNewtonsoft
     public class ListTests
     {
         [Test]
-        public void Deserialize_PartialList_ReturnsList()
+        public void DeserializeObject_JsonList_ReturnsList()
         {
             // Assemble
             string json = @"
-                {'Number':'1','Letter':'A'},
-                {'Number':'2','Letter':'B'},
-                {'Number':'3','Letter':'C'},";
+                [
+                    {'Number':'1','Letter':'A'},
+                    {'Number':'2','Letter':'B'},
+                    {'Number':'3','Letter':'C'}
+                ]";
 
             // Act
-            List<Data> actual = null;
+            List<Data> actual = JsonConvert.DeserializeObject<List<Data>>(json);
+
+            // Assert
+            Assert.AreEqual(1, actual[0].Number);
+            Assert.AreEqual("A", actual[0].Letter);
+
+            Assert.AreEqual(2, actual[1].Number);
+            Assert.AreEqual("B", actual[1].Letter);
+
+            Assert.AreEqual(3, actual[2].Number);
+            Assert.AreEqual("C", actual[2].Letter);
+        }
+
+        [Test]
+        public void DeserializeObject_MissingSquareBracesAroundJsonList_Throws()
+        {
+            // Assemble
+            string json = @"
+                    {'Number':'1','Letter':'A'},
+                    {'Number':'2','Letter':'B'},
+                    {'Number':'3','Letter':'C'}";
+
+            // Act
+            Assert.Throws<JsonSerializationException>(() => JsonConvert.DeserializeObject<List<Data>>(json));
+        }
+
+        [Test]
+        public void DeserializeObject_TrailingCommaAtEndOfJsonList_ReturnsList()
+        {
+            // Assemble
+            string json = @"
+                [
+                    {'Number':'1','Letter':'A'},
+                    {'Number':'2','Letter':'B'},
+                    {'Number':'3','Letter':'C'},
+                ]";
+
+            // Act
+            List<Data> actual = JsonConvert.DeserializeObject<List<Data>>(json);
 
             // Assert
             Assert.AreEqual(1, actual[0].Number);
