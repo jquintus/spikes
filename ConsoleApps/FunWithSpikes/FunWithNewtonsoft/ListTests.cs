2d44a1ac53ee32fa5ea1ca75dc7824f5a5d45606
jquintus <josh@dotalign.com>
2016-01-19 10:19:57 -0500

    Add tests IEnumerable

--- ConsoleApps/FunWithSpikes/FunWithNewtonsoft/ListTests.cs
+++ ConsoleApps/FunWithSpikes/FunWithNewtonsoft/ListTests.cs
@@ -1,12 +1,38 @@
 ﻿using Newtonsoft.Json;
 using NUnit.Framework;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FunWithNewtonsoft
 {
     [TestFixture]
     public class ListTests
     {
+        [Test]
+        public void DeserializeObject_JsonList_ReturnsIEnumerable()
+        {
+            // Assemble
+            string json = @"
+                [
+                    {'Number':'1','Letter':'A'},
+                    {'Number':'2','Letter':'B'},
+                    {'Number':'3','Letter':'C'}
+                ]";
+
+            // Act
+            List<Data> actual = JsonConvert.DeserializeObject<IEnumerable<Data>>(json).ToList();
+
+            // Assert
+            Assert.AreEqual(1, actual[0].Number);
+            Assert.AreEqual("A", actual[0].Letter);
+
+            Assert.AreEqual(2, actual[1].Number);
+            Assert.AreEqual("B", actual[1].Letter);
+
+            Assert.AreEqual(3, actual[2].Number);
+            Assert.AreEqual("C", actual[2].Letter);
+        }
+
         [Test]
         public void DeserializeObject_JsonList_ReturnsList()
         {
