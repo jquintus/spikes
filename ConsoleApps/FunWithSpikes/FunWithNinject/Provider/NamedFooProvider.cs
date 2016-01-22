b82948ffad2c95edd485ae0bc977e33a331d7480
jquintus <josh@dotalign.com>
2016-01-22 15:33:04 -0500

    Fixing file names

--- ConsoleApps/FunWithSpikes/FunWithNinject/Provider/NamedFooProvider.cs
+++ ConsoleApps/FunWithSpikes/FunWithNinject/Provider/NamedFooProvider.cs
@@ -1,92 +1,47 @@
-﻿using Ninject;
-using NUnit.Framework;
+using Ninject;
+using Ninject.Activation;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FunWithNinject.Provider
 {
-    [TestFixture]
-    public class ProviderTests
+    public class NamedFooProvider : Provider<IFoo>
     {
-        [Test]
-        public void Get_DirectDependency_InjectsMultipleInstancesOfIFoo()
+        public const string Foo1 = "First";
+        public const string Foo2 = "Second";
+        private readonly IFoo _first;
+        private readonly IFoo _second;
+
+        public NamedFooProvider(
+        [Named(Foo1)]IFoo firstFoo,
+        [Named(Foo2)]IFoo secondFoo)
         {
-            using (var k = CreateKernel())
+            _first = firstFoo;
+            _second = secondFoo;
+        }
+
+        protected override IFoo CreateInstance(IContext context)
+        {
+            var name = GetContexts(context)
+                .Select(c => c.Binding?.Metadata?.Name)
+                .First(n => n != null);
+
+            if (name == Foo2)
             {
-                // Assemble
-                k.Bind<IFoo>().ToProvider<NamedFooProvider>().Named(NamedFooProvider.Foo1);
-                k.Bind<IFoo>().ToProvider<NamedFooProvider>().Named(NamedFooProvider.Foo2);
-
-                // Act
-                var direct = k.Get<DirectDualDependency>();
-
-                // Assert
-                Assert.IsInstanceOf<Foo1>(direct.FirstFoo);
-                Assert.IsInstanceOf<Foo2>(direct.SecondFoo);
+                return _second;
+            }
+            {
+                return _first;
             }
         }
 
-        [Test]
-        public void Get_IndirectDependencyOnFoo1_InjectsInstanceOfFoo1()
+        private IEnumerable<IContext> GetContexts(IContext context)
         {
-            using (var k = CreateKernel())
+            while (context != null)
             {
-                // Assemble
-                k.Bind<DirectDependency>().ToSelf();
-                k.Bind<DirectDependency>().ToSelf().Named(NamedFooProvider.Foo1);
-                k.Bind<DirectDependency>().ToSelf().Named(NamedFooProvider.Foo2);
-
-                // Act
-                var indirect = k.Get<IndirectDependencyOnFoo1>();
-
-                // Assert
-                Assert.IsInstanceOf<Foo1>(indirect.DirectDependency.Foo);
+                yield return context;
+                context = context.Request.ParentContext;
             }
-        }
-
-        [Test]
-        public void Get_IndirectDependencyOnFoo2_InjectsInstanceOfFoo2()
-        {
-            using (var k = CreateKernel())
-            {
-                // Assemble
-                k.Bind<DirectDependency>().ToSelf();
-                k.Bind<DirectDependency>().ToSelf().Named(NamedFooProvider.Foo1);
-                k.Bind<DirectDependency>().ToSelf().Named(NamedFooProvider.Foo2);
-
-                // Act
-                var indirect = k.Get<IndirectDependencyOnFoo2>();
-
-                // Assert
-                Assert.IsInstanceOf<Foo2>(indirect.DirectDependency.Foo);
-            }
-        }
-
-        [Test]
-        public void Get_MoreIndirectDependencyOnFoo1_InjectsInstanceOfFoo1()
-        {
-            using (var k = CreateKernel())
-            {
-                // Assemble
-                k.Bind<IntermediateDependency>().ToSelf();
-                k.Bind<IntermediateDependency>().ToSelf().Named(NamedFooProvider.Foo1);
-                k.Bind<IntermediateDependency>().ToSelf().Named(NamedFooProvider.Foo2);
-
-                // Act
-                var indirect = k.Get<MoreIndirectDependencyOnFoo1>();
-
-                // Assert
-                Assert.IsInstanceOf<Foo1>(indirect.IntermediateDependency.DirectDependency.Foo);
-            }
-        }
-
-        private IKernel CreateKernel()
-        {
-            var k = new StandardKernel();
-            k.Bind<IFoo>().To<Foo1>().WhenInjectedExactlyInto<NamedFooProvider>().Named(NamedFooProvider.Foo1);
-            k.Bind<IFoo>().To<Foo2>().WhenInjectedExactlyInto<NamedFooProvider>().Named(NamedFooProvider.Foo2);
-
-            k.Bind<IFoo>().ToProvider<NamedFooProvider>();
-
-            return k;
         }
     }
 }
---
+++ ConsoleApps/FunWithSpikes/FunWithNinject/Provider/ProviderTests.cs
@@ -0,0 +1,92 @@
+﻿using Ninject;
+using NUnit.Framework;
+
+namespace FunWithNinject.Provider
+{
+    [TestFixture]
+    public class ProviderTests
+    {
+        [Test]
+        public void Get_DirectDependency_InjectsMultipleInstancesOfIFoo()
+        {
+            using (var k = CreateKernel())
+            {
+                // Assemble
+                k.Bind<IFoo>().ToProvider<NamedFooProvider>().Named(NamedFooProvider.Foo1);
+                k.Bind<IFoo>().ToProvider<NamedFooProvider>().Named(NamedFooProvider.Foo2);
+
+                // Act
+                var direct = k.Get<DirectDualDependency>();
+
+                // Assert
+                Assert.IsInstanceOf<Foo1>(direct.FirstFoo);
+                Assert.IsInstanceOf<Foo2>(direct.SecondFoo);
+            }
+        }
+
+        [Test]
+        public void Get_IndirectDependencyOnFoo1_InjectsInstanceOfFoo1()
+        {
+            using (var k = CreateKernel())
+            {
+                // Assemble
+                k.Bind<DirectDependency>().ToSelf();
+                k.Bind<DirectDependency>().ToSelf().Named(NamedFooProvider.Foo1);
+                k.Bind<DirectDependency>().ToSelf().Named(NamedFooProvider.Foo2);
+
+                // Act
+                var indirect = k.Get<IndirectDependencyOnFoo1>();
+
+                // Assert
+                Assert.IsInstanceOf<Foo1>(indirect.DirectDependency.Foo);
+            }
+        }
+
+        [Test]
+        public void Get_IndirectDependencyOnFoo2_InjectsInstanceOfFoo2()
+        {
+            using (var k = CreateKernel())
+            {
+                // Assemble
+                k.Bind<DirectDependency>().ToSelf();
+                k.Bind<DirectDependency>().ToSelf().Named(NamedFooProvider.Foo1);
+                k.Bind<DirectDependency>().ToSelf().Named(NamedFooProvider.Foo2);
+
+                // Act
+                var indirect = k.Get<IndirectDependencyOnFoo2>();
+
+                // Assert
+                Assert.IsInstanceOf<Foo2>(indirect.DirectDependency.Foo);
+            }
+        }
+
+        [Test]
+        public void Get_MoreIndirectDependencyOnFoo1_InjectsInstanceOfFoo1()
+        {
+            using (var k = CreateKernel())
+            {
+                // Assemble
+                k.Bind<IntermediateDependency>().ToSelf();
+                k.Bind<IntermediateDependency>().ToSelf().Named(NamedFooProvider.Foo1);
+                k.Bind<IntermediateDependency>().ToSelf().Named(NamedFooProvider.Foo2);
+
+                // Act
+                var indirect = k.Get<MoreIndirectDependencyOnFoo1>();
+
+                // Assert
+                Assert.IsInstanceOf<Foo1>(indirect.IntermediateDependency.DirectDependency.Foo);
+            }
+        }
+
+        private IKernel CreateKernel()
+        {
+            var k = new StandardKernel();
+            k.Bind<IFoo>().To<Foo1>().WhenInjectedExactlyInto<NamedFooProvider>().Named(NamedFooProvider.Foo1);
+            k.Bind<IFoo>().To<Foo2>().WhenInjectedExactlyInto<NamedFooProvider>().Named(NamedFooProvider.Foo2);
+
+            k.Bind<IFoo>().ToProvider<NamedFooProvider>();
+
+            return k;
+        }
+    }
+}
