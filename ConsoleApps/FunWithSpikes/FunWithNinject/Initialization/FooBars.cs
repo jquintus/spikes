9b97aaa2c12c6561065047d3a577fba49058e7ce
Josh Quintus <josh@dotalign.com>
2017-05-24 16:45:42 -0400

    Figuring out CTOR arguments with ninject

---
+++ ConsoleApps/FunWithSpikes/FunWithNinject/Initialization/FooBars.cs
@@ -0,0 +1,23 @@
+﻿namespace FunWithNinject.Initialization
+{
+    public class FooBars
+    {
+        public interface IFoo
+        {
+            string ExtraInfo { get; }
+        }
+        public interface IFoo1 : IFoo { }
+        public interface IFoo2 : IFoo { }
+        public interface IBar { }
+        public class Bar : IBar { }
+        public class Foo : IFoo1, IFoo2
+        {
+            public Foo(IBar bar, string extraInfo)
+            {
+                ExtraInfo = extraInfo;
+            }
+
+            public string ExtraInfo { get; }
+        }
+    }
+}
--- ConsoleApps/FunWithSpikes/FunWithNinject/Initialization/Tests.cs
+++ ConsoleApps/FunWithSpikes/FunWithNinject/Initialization/Tests.cs
@@ -1,12 +1,35 @@
-﻿namespace FunWithNinject.Initialization
-{
-    using Ninject;
-    using NUnit.Framework;
-    using System.Threading;
+﻿using Ninject;
+using NUnit.Framework;
+using System.Threading;
 
+namespace FunWithNinject.Initialization
+{
     [TestFixture]
     public class Tests
     {
+        [Test]
+        public void BindWithConstructorArgument_PassedInAsAdditionalArgument()
+        {
+            // Assemble
+            using (var k = new StandardKernel())
+            {
+                const string extra1 = "Extra Data For Foo1";
+                const string extra2 = "Extra Data For Foo2:  I pity the FOO1";
+
+                k.Bind<FooBars.IBar>().To<FooBars.Bar>();
+                k.Bind<FooBars.IFoo1>().To<FooBars.Foo>().WithConstructorArgument(extra1);
+                k.Bind<FooBars.IFoo2>().To<FooBars.Foo>().WithConstructorArgument(extra2);
+
+                // Act
+                var foo1 = k.Get<FooBars.IFoo1>();
+                var foo2 = k.Get<FooBars.IFoo2>();
+
+                // Assert
+                Assert.AreEqual(extra1, foo1.ExtraInfo);
+                Assert.AreEqual(extra2, foo2.ExtraInfo);
+            }
+        }
+
         [Test]
         public void OnActivation_CanCallMethodOnImplementation()
         {
