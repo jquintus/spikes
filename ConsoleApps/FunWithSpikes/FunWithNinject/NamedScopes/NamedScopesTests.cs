56dfd551b50da6659f6f11b8225f102b669187f6
Josh Quintus <josh@dotalign.com>
2017-09-29 16:31:58 -0400

    Test what happens when we get singltons involved with context preservation and named scope

--- ConsoleApps/FunWithSpikes/FunWithNinject/NamedScopes/NamedScopesTests.cs
+++ ConsoleApps/FunWithSpikes/FunWithNinject/NamedScopes/NamedScopesTests.cs
@@ -32,5 +32,32 @@ namespace FunWithNinject.NamedScopes
                 Assert.AreEqual(root1.EventAggregator, root1.ChildEventAggregator);
             }
         }
+
+
+        [Test]
+        public void Get_FuncBoundInNamedScopeHeldBySingletonScopedItem_FunctionOnlyResolvedOnce()
+        {
+            // This test depends on Ninject.Extensions.ContextPreservation being loaded.
+
+            // Assemble
+            using (var k = new StandardKernel())
+            {
+                const string scope = "Telescope";
+                k.Bind<IRoot>().To<Root>().DefinesNamedScope(scope);
+                k.Bind<IHaveFunc>().To<HaveFunc>().InSingletonScope();
+                k.Bind<IEventAggregator>().To<EventAggregator>().InNamedScope(scope);
+
+                // Act
+                var root1 = k.Get<IRoot>();
+                var root2 = k.Get<IRoot>();
+
+                // Assert
+
+                Assert.AreNotEqual(root1, root2);
+                Assert.AreEqual(root1.ChildFuncHaver, root2.ChildFuncHaver);
+                Assert.AreEqual(root1.ChildEventAggregator, root2.ChildEventAggregator);
+                Assert.AreEqual(root1.EventAggregator, root1.ChildEventAggregator);
+            }
+        }
     }
 }
