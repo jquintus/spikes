b70744cc55f4dbf1f130eaecba67764c4ea8d23b
Josh Quintus <josh.quintus@gmail.com>
2017-10-08 22:47:47 -0400

    Playign around with banchmarking

---
+++ ConsoleApps/FunWithSpikes/FunWithBenchmarks/Program.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace FunWithBenchmarks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var switcher = new BenchmarkSwitcher(new[] {
+                typeof(Md5VsSha256),
+                typeof(VirtualFun),
+            });
+            switcher.Run(args);
+
+
+            if (Debugger.IsAttached)
+            {
+                Console.WriteLine("Press enter");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ConsoleApps/FunWithSpikes/FunWithBenchmarks/VirtualFun.cs
@@ -0,0 +1,88 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace FunWithBenchmarks
+{
+    public class VirtualFun
+    {
+        private readonly BaseClass _abstractClass;
+        private readonly ISomething _interface;
+        private readonly TestClass _testClass;
+
+        public VirtualFun()
+        {
+            _testClass = new TestClass();
+            _abstractClass = new TestClass();
+            _interface = new TestClass();
+        }
+
+        [Benchmark]
+        public void BenchmarkAbstract()
+        {
+            _abstractClass.AbstractMethod();
+        }
+
+        [Benchmark]
+        public void BenchmarkInterface()
+        {
+            _interface.InterfaceMethod();
+        }
+
+        [Benchmark(Baseline = true)]
+        public void BenchmarkSealed()
+        {
+            _testClass.SealedMethod();
+        }
+
+        [Benchmark]
+        public void TimeStatic()
+        {
+            TestClass.StaticMethod();
+        }
+
+        [Benchmark]
+        public void TimeVirtual()
+        {
+            _testClass.VirtualMethod();
+        }
+
+        #region Nested Classes
+
+        private interface ISomething
+        {
+            void InterfaceMethod();
+        }
+
+        public abstract class BaseClass
+        {
+            public abstract void AbstractMethod();
+        }
+
+        public class TestClass : BaseClass, ISomething
+        {
+            [Benchmark]
+            public static void StaticMethod()
+            {
+            }
+
+            [Benchmark]
+            public override void AbstractMethod()
+            {
+            }
+
+            [Benchmark]
+            public void InterfaceMethod()
+            {
+            }
+
+            public void SealedMethod()
+            {
+            }
+
+            public virtual void VirtualMethod()
+            {
+            }
+        }
+
+        #endregion Nested Classes
+    }
+}
