70c95ac1ce3fa3e6ce299e51bf8520f61c331756
Josh Quintus <josh.quintus@gmail.com>
2018-01-08 21:02:12 -0500

    Create test project for castle core play

---
+++ ConsoleApps/FunWithSpikes/FunWithCastles/Class1.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithCastles
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Fail();
+        }
+    }
+}
