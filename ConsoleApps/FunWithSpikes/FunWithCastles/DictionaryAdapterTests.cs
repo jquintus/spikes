f874650a790fcc6aeb426b5f26158d790597433f
Josh Quintus <josh.quintus@gmail.com>
2018-01-08 21:33:11 -0500

    Learned about DictionaryAdapter

---
+++ ConsoleApps/FunWithSpikes/FunWithCastles/DictionaryAdapterTests.cs
@@ -0,0 +1,126 @@
+﻿using Castle.Components.DictionaryAdapter;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace FunWithCastles
+{
+    [TestFixture]
+    public class DictionaryAdapterTests
+    {
+        private const string MAX = nameof(ISettings.MaxItems);
+        private const string NAME = nameof(ISettings.Name);
+
+        [Test]
+        public void Get_ReferenceTypeDoesNotExistsInDictionary_ReturnsNull()
+        {
+            // Assemble
+            var dictionary = new Dictionary<string, string>();
+            var adapter = CreateSettings(dictionary);
+
+            // Act
+            var name = adapter.Name;
+
+            // Assert
+            Assert.IsNull(name);
+        }
+
+        [Test]
+        public void Get_ValueExistsInDictionary_ReturnsCorrectValue()
+        {
+            // Assemble
+            var dictionary = new Dictionary<string, string>
+            {
+                [MAX] = "42",
+                [NAME] = "Douglas",
+            };
+
+            var adapter = CreateSettings(dictionary);
+
+            // Act
+            var max = adapter.MaxItems;
+            var name = adapter.Name;
+
+            // Assert
+            Assert.AreEqual(42, max);
+            Assert.AreEqual("Douglas", name);
+        }
+
+        [Test]
+        public void Get_ValueTypeDoesNotExistsInDictionary_ReturnsDefault()
+        {
+            // Assemble
+            var dictionary = new Dictionary<string, string>();
+            var adapter = CreateSettings(dictionary);
+
+            // Act
+            var max = adapter.MaxItems;
+
+            // Assert
+            Assert.AreEqual(0, max);
+        }
+
+        [Test]
+        public void Set_ValueDoesNotExistInDictionary_AddsValue()
+        {
+            // Assemble
+            var dictionary = new Dictionary<string, string>();
+            var adapter = CreateSettings(dictionary);
+
+            // Act
+            adapter.Name = "Douglas";
+
+            // Assert
+            Assert.AreEqual("Douglas", dictionary[NAME]);
+        }
+
+        [Test]
+        public void Set_ValueExistsInDictionary_OverWritesValue()
+        {
+            // Assemble
+            var dictionary = new Dictionary<string, string>
+            {
+                [NAME] = "Douglas",
+            };
+            var adapter = CreateSettings(dictionary);
+
+            // Act
+            adapter.Name = "Adams";
+
+            // Assert
+            Assert.AreEqual("Adams", dictionary[NAME]);
+        }
+
+        [Test]
+        public void Set_ValueIsNotAStringAndDictionaryIsNotTyped_SavesValue()
+        {
+            // Assemble
+            var dictionary = new System.Collections.Hashtable();
+            var adapter = CreateSettings(dictionary);
+
+            // Act
+            adapter.MaxItems = 42;
+
+            // Assert
+            Assert.AreEqual(42, dictionary[MAX]);
+        }
+
+        [Test]
+        public void Set_ValueIsNotAStringAndDictionaryIsTyped_ThrowsArgumentException()
+        {
+            // Assemble
+            var dictionary = new Dictionary<string, string>();
+            var adapter = CreateSettings(dictionary);
+
+            // Act
+            Assert.Throws<ArgumentException>(() => adapter.MaxItems = 42);
+        }
+
+        private static ISettings CreateSettings(System.Collections.IDictionary dictionary)
+        {
+            var factory = new DictionaryAdapterFactory();
+            var adapter = factory.GetAdapter<ISettings>(dictionary);
+            return adapter;
+        }
+    }
+}
---
+++ ConsoleApps/FunWithSpikes/FunWithCastles/ISettings.cs
@@ -0,0 +1,8 @@
+﻿namespace FunWithCastles
+{
+    public interface ISettings
+    {
+        int MaxItems { get; set; }
+        string Name { get; set; }
+    }
+}
