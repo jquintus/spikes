32caf479cc8b522d44cc70a4216fea83e502a6db
Josh Quintus <josh.quintus@gmail.com>
2018-01-09 20:35:19 -0500

    Implemented defaults via expressions

---
+++ ConsoleApps/FunWithSpikes/FunWithCastles/Settings/LinqExt.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace FunWithCastles.Settings
+{
+    public static class LinqExt
+    {
+        public static IEnumerable<T> Prepend<T> (this IEnumerable<T> src, T toAdd)
+        {
+            yield return toAdd;
+
+            if (src!= null)
+            {
+                foreach (var item in src)
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
