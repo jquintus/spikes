5a8892b4a4e6957e6cd1dc2ff4c78e560b16cda8
Josh Quintus <josh.quintus@gmail.com>
2018-01-09 20:03:40 -0500

    Add readonly builder extension methods

---
+++ ConsoleApps/FunWithSpikes/FunWithCastles/Settings/Adapters/MemorySettingsBuilderExt.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+namespace FunWithCastles.Settings.Adapters
+{
+    public static class MemorySettingsBuilderExt
+    {
+        public static SettingsBuilder AddMemoryAdapter(
+            this SettingsBuilder builder,
+            Hashtable data = null)
+        {
+            return builder.Add(new MemoryAdapter(data));
+        }
+
+        public static SettingsBuilder AddReadOnlyMemoryAdapter(
+            this SettingsBuilder builder,
+            Hashtable data = null)
+        {
+            return builder.AddReadOnly(new MemoryAdapter(data));
+        }
+    }
+}
---
+++ ConsoleApps/FunWithSpikes/FunWithCastles/Settings/Adapters/RegistrySettingsBuilderExt.cs
@@ -0,0 +1,19 @@
+﻿namespace FunWithCastles.Settings.Adapters
+{
+    public static class RegistrySettingsBuilderExt
+    {
+        public static SettingsBuilder AddReadOnlyRegistryAdapter(
+            this SettingsBuilder builder,
+            string root)
+        {
+            return builder.AddReadOnly(new RegistryAdapter(root));
+        }
+
+        public static SettingsBuilder AddRegistryAdapter(
+                    this SettingsBuilder builder,
+            string root)
+        {
+            return builder.Add(new RegistryAdapter(root));
+        }
+    }
+}
