10be9911592a06c938723a3330f0af71a8bd9643
Josh Quintus <josh.quintus@gmail.com>
2018-01-09 20:03:40 -0500

    Add readonly builder extension methods

---
+++ ConsoleApps/FunWithSpikes/FunWithCastles/Settings/Adapters/EnvironmentVariablSettingsBuilderExt.cs
@@ -0,0 +1,19 @@
+﻿namespace FunWithCastles.Settings.Adapters
+{
+    public static class EnvironmentVariablSettingsBuilderExt
+    {
+        public static SettingsBuilder AddEnvironmentVariableAdapter(
+            this SettingsBuilder builder,
+            string prefix = null)
+        {
+            return builder.Add(new EnvironmentVariableAdapter(prefix));
+        }
+
+        public static SettingsBuilder AddReadOnlyEnvironmentVariableAdapter(
+            this SettingsBuilder builder,
+            string prefix = null)
+        {
+            return builder.AddReadOnly(new EnvironmentVariableAdapter(prefix));
+        }
+    }
+}
