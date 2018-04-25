fd15f13d53532f8f427c6073e1a394a5e388e212
Josh Quintus <josh@dotalign.com>
2018-04-25 11:32:14 -0400

    Add tests for varience and ninject scoping

---
+++ ConsoleApps/FunWithSpikes/FunWithNinject/Scope/ScopeTests.cs
@@ -0,0 +1,70 @@
+﻿using Ninject;
+using NUnit.Framework;
+
+namespace FunWithNinject.Scope
+{
+    public class ScopeTests
+    {
+        #region Helper classes
+
+        public interface IScare
+        {
+            void Scare();
+        }
+
+        public class Boo : IScare
+        {
+            public void Scare()
+            {
+                System.Console.WriteLine("Boo");
+            }
+        }
+
+        public class RealMonster
+        {
+            public RealMonster(IScare scare1, IScare scare2)
+            {
+                Scare1 = scare1;
+                Scare2 = scare2;
+            }
+
+            public IScare Scare1 { get; }
+            public IScare Scare2 { get; }
+        }
+
+        #endregion Helper classes
+
+        [Test]
+
+        public void MultipleInstancesPassedToOneConstructor_SingletonScope_InstancesAreSame()
+        {
+            // Assemble
+            using (var k = new StandardKernel())
+            {
+                k.Bind<IScare>().To<Boo>().InSingletonScope();
+
+                // Act
+                var monster = k.Get<RealMonster>();
+
+                // Assert
+                Assert.AreSame(monster.Scare1, monster.Scare2);
+            }
+        }
+
+        [Test]
+        public void MultipleInstancesPassedToOneConstructor_TransientScope_InstancesDiffer()
+        {
+            // Assemble
+            using (var k = new StandardKernel())
+            {
+                k.Bind<IScare>().To<Boo>().InTransientScope();
+
+                // Act
+                var monster = k.Get<RealMonster>();
+
+                // Assert
+                Assert.AreNotSame(monster.Scare1, monster.Scare2);
+            }
+        }
+    }
+}
---
+++ ConsoleApps/FunWithSpikes/FunWithSpikes/GenericVariance.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+namespace FunWithSpikes
+{
+    [TestFixture]
+    public class GenericVariance
+    {
+        #region Classes
+
+        public interface IBar<out TFoo> where TFoo : IFoo
+        {
+            TFoo Foo { get; }
+        }
+
+        public interface IFoo { }
+
+        public class Bar<TFoo> : IBar<TFoo> where TFoo : IFoo
+        {
+            public Bar(TFoo foo)
+            {
+                Foo = foo;
+            }
+
+            public TFoo Foo { get; }
+        }
+
+        public class Foo : IFoo { }
+
+        #endregion Classes
+
+        [Test]
+        public void Cast_TypeHasConcreteGenericParameter_Succeeds()
+        {
+            // Assemble
+            var specificBar = new Bar<Foo>(new Foo());
+
+            // Act (we don't want this to throw)
+            var genericBar = (IBar<IFoo>)specificBar;
+        }
+    }
+}
