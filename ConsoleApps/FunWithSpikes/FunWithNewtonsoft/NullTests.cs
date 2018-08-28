144b5fd143d8b091d4cdca7b437deea950369685
Josh Quintus <josh@dotalign.com>
2018-08-28 09:47:35 -0400

    Add test to verify deserialization of nulls

---
+++ ConsoleApps/FunWithSpikes/FunWithNewtonsoft/NullTests.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace FunWithNewtonsoft
+{
+    [TestFixture]
+    public class NullTests
+    {
+        [Test]
+        public void DeserializeObject_NullValueInJsonString_SetsPropertyToNull()
+        {
+            // Assemble
+            string json = @"{'Number':'1','Letter':null}";
+
+            // Act
+            var actual = JsonConvert.DeserializeObject<Data>(json);
+
+            // Assert
+            Assert.IsNull(actual.Letter);
+        }
+    }
+}
